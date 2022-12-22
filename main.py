723f51d62fea96fa3e6b628aa41ee1ee56c866c8
Alexey Alexeev <eurodeveloper@ya.ru>
2022-12-22 20:43:47 +0200

    добавили index.html и base.html

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
+
+
+messanger = ['whatsapp', 'viber', 'telegram']
+hat = ['главная', 'каталог', 'ДС', 'выйти', 'войти', 'пользователь',
+       'корзина', 'регистрация']
 
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-    return 'test'
+    return render_template('index.html', messanger=messanger, hat=hat)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
